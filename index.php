e6ffa3f152ac343f8b1cd34c24377ded42a7b43c
brucordeiro <brunapcordeiro7@gmail.com>
2024-10-30 07:57:55 -0300

    Imagem adicionada

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<!DOCTYPE html> 
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -6,6 +6,7 @@
     <title>Document</title>
 </head>
 <body>
-    <h1> Ana Júlia e Bruna Cordeiro </h1> 
+    <h1> Ana Júlia e Bruna Cordeiro </h1>
+    <img src="\ProvaGitBJ\imagens\pizza.jpg">
 </body>
 </html>
